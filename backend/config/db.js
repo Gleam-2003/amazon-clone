3774591b16a0ad1425b5074de3ab1cb700916afa
Rahul Kumar Behura <rahulbehura049@gmail.com>
2026-01-11 14:44:07 +0530

    backend db update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,32 +1,11 @@
-// const mysql = require("mysql2");
-// require("dotenv").config();
-
-// const db = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
-// db.connect((err) => {
-//   if (err) {
-//     console.error("❌ Database connection failed:", err.message);
-//   } else {
-//     console.log("✅ MySQL Connected");
-//   }
-// });
-
-// module.exports = db;
-
-
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  host: process.env.DB_HOST,        // ✅ mysql.railway.internal
+  user: process.env.DB_USER,        // ✅ root
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,    // ✅ railway
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
 });
